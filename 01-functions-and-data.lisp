34374fc86b7c3157276e7ae1f624251db9f821a0
philbaker <phil@philbaker.me>
2023-07-24 22:38:56 +0100

    initial commit

--- 01-functions-and-data.lisp
+++ 01-functions-and-data.lisp
@@ -1,1 +1,18 @@
-(+ 1 1)
+; Exercises
+
+; 1.4 Define a SUB2 function that subtracts two from its input
+(defun SUB1 (n)
+  (- n 1))
+
+(defun SUB2 (n)
+  (SUB1 (SUB1 n)))
+
+(SUB2 5)
+; 3
+
+; 1.5 Show how to write TWOP in terms of ZEROP and SUB2
+(defun TWOP (n)
+  (zerop (SUB2 n)))
+
+(TWOP 2)
+; T
