c8872d1d8f85b6e0a50321b697a2595985c9cb7e
philbaker <phil@philbaker.me>
2023-07-28 05:59:27 +0100

    Add 2.3

--- 02-lists.lisp
+++ 02-lists.lisp
@@ -1,6 +1,20 @@
 ; 2.1 show how the list (TO BE OR NOT TO BE) would be reperesented in computer
 ; memory by drawing its cons cell representation
-; [ TO | -> ] [ BE | -> ] [ OR | -> ] [ NOT | -> ] [ TO | -> ] [ BE | NIL ]
+; [.|.]->[BE|.]->[OR|.]->[NOT|.]->[TO|.]->[BE|.]->NIL
 
 
+; 2.2 which of these are well-formed lists?
+
+; ('A 'B ('C) - no
+; (('A) ('B)) - yes
+; 'A 'B ) ('C 'D) - no
+; ('A ('B ('C)) - no
+; ('A ('B ('C))) - yes
+; ((('A) ('B)) ('C)) - yes
+
+
+; 2.3 draw the cons cell representation of the list (PLEASE (BE MY) VALENTINE)
+; [PLEASE|.]->[.|.]->[VALENTINE|.]->NIL
+;              |
+;             [BE|.]->[MY|.]->NIL
 
