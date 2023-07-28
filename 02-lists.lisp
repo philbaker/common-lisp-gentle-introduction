bce81854fb20aa4cbd2b38b3601d3ad1bd620319
philbaker <phil@philbaker.me>
2023-07-28 05:36:44 +0100

    Add initial chapter 2 exercise

---
+++ 02-lists.lisp
@@ -0,0 +1,6 @@
+; 2.1 show how the list (TO BE OR NOT TO BE) would be reperesented in computer
+; memory by drawing its cons cell representation
+; [ TO | -> ] [ BE | -> ] [ OR | -> ] [ NOT | -> ] [ TO | -> ] [ BE | NIL ]
+
+
+
