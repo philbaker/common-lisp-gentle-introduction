06f0b85ce3d4d3192b68d5a3b069087cf31b4c65
philbaker <phil@philbaker.me>
2023-07-31 05:56:55 +0100

    Add latest exercises from chapter 2

--- 02-lists.lisp
+++ 02-lists.lisp
@@ -107,6 +107,71 @@
 
 ; 2.11 draw the cons cell representation of the list (A (TOLL) ((CALL)))
 
-; [A|.]->[.|.]->[.|.]
-;  ...
+; [A|.]->[.|.]---------->[.|.]->NIL
+;         |               |
+;        [TOLL\.]->NIL   [.|.]->NIL
+;                         |
+;                        [CALL|.]->NIL
 
+
+; 2.12 what c...r name does lisp use for the function that returns the fourth
+; element of a list? how would you prononce it?
+(cadddr '(a b c d e))
+; D
+; "ka-dih-dih-der"
+
+
+; 2.13 write down a table to illustrate how to get to each word in the list
+; (((FUN)) (IN THE) (SUN))
+(car '(((FUN)) (IN THE) (SUN))) ; ((FUN))
+(caar '(((FUN)) (IN THE) (SUN))) ; (FUN)
+(caaar '(((FUN)) (IN THE) (SUN))) ; FUN
+
+(cdr '(((FUN)) (IN THE) (SUN))) ; ((IN THE) (SUN))
+(cadr '(((FUN)) (IN THE) (SUN))) ; (IN THE)
+(caadr '(((FUN)) (IN THE) (SUN))) ; IN
+(cadadr '(((FUN)) (IN THE) (SUN))) ; THE
+
+(cddr '(((FUN)) (IN THE) (SUN))) ; ((SUN))
+(caddr '(((FUN)) (IN THE) (SUN))) ; (SUN)
+(caaddr '(((FUN)) (IN THE) (SUN))) ; SUN
+
+
+; 2.14 what would happen if you tried to explain the operation of the CAADR
+; function on the list ((BLUE CUBE) (RED PYRAMID)) by reading the As and Ds
+; from left to right instead of right to left?
+(caadr '((BLUE CUBE) (RED PYRAMID))) ; RED
+; (cdaar '((BLUE CUBE) (RED PYRAMID))) ; The value BLUE is not of type LIST
+
+
+; 2.15 using the list ((A B) (C D) (E F)), fill in the missing pairs of this table
+(defun cadaddr (l)
+  (car (cdaddr l)))
+
+(car '((A B) (C D) (E F))) ; (A B)
+(cddr '((A B) (C D) (E F))) ; ((E F))
+(cadr '((A B) (C D) (E F))) ; (C D)
+(cdar '((A B) (C D) (E F))) ; (B)
+(cadar '((A B) (C D) (E F))) ; B
+(cddar '((A B) (C D) (E F))) ; NIL
+(caar '((A B) (C D) (E F))) ; A
+(cdaddr '((A B) (C D) (E F))) ; (F)
+(cadaddr '((A B) (C D) (E F))) ; F
+
+
+; 2.16 what does CAAR do when given the input (FRED NIL)?
+; (caar '(FRED NIL)) ; The value FRED is not of type LIST
+
+
+; 2.17 fill in the results of the following computations
+(car '(POST NO BILLS)) ; POST
+(cdr '(POST NO BILLS)) ; (NO BILLS)
+(car '((POST NO) BILLS)) ; (POST NO)
+(cdr '(BILLS)) ; NIL
+; (car 'BILLS) ; The value BILLS is not of type LIST when binding LIST
+(cdr '(POST (NO BILLS))) ; ((NO BILLS))
+(cdr '((POST NO BILLS))) ; NIL
+(car nil) ; NIL
+
+; cons
+(cons 'FROB nil) ; (FROB)
