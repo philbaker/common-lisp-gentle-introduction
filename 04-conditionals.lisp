0215471ee83563b845d29136bb4e9167c6498e58
philbaker <phil@philbaker.me>
2023-08-08 07:28:53 +0100

    Add latest chapter 4 exercises

--- 04-conditionals.lisp
+++ 04-conditionals.lisp
@@ -140,3 +140,117 @@
 
 (emphasize3 '(very long day))
 ; (VERY VERY LONG DAY)
+
+
+; 4.9 what is wrong with the following function?
+; (defun make-odd (x)
+;   (cond (t x)
+;         ((not oddp x)) (+ 1 x)))
+
+; (make-odd 3)
+; 3
+
+; (make-odd 4)
+; 4
+
+; (make-odd -2)
+; -2
+
+(defun make-odd (x)
+  (cond ((not (oddp x)) (+ 1 x))
+        (t x)))
+
+(make-odd 3)
+; 3
+
+(make-odd 4)
+; 5
+
+(make-odd -2)
+; -1
+
+
+; 4.10 write a function CONSTRAIN that takes X, MAX, and min
+(defun constrain-cond (x max min)
+  (cond ((< x min) min)
+        ((> x max) max)
+        (t x)))
+
+(constrain-cond 3 -50 -50)
+; -50
+
+(constrain-cond 92 -50 50)
+; -50
+
+(defun constrain-if (x max min)
+  (if (< x min) min
+    (if (> x max) max
+      x)))
+
+(constrain-if 3 -50 -50)
+; -50
+
+(constrain-if 92 -50 50)
+; -50
+
+
+; 4.11 write a function FIRSTZERO that takes a list of three numbers as input
+; and returns a word "first", "second", "third" or "none" based on where zero
+; is in the list
+(defun firstzero (l)
+  (cond ((equal (first l) 0) 'first)
+        ((equal (second l) 0) 'second)
+        ((equal (third l) 0) 'third)
+        (t 'none)))
+
+(firstzero '(0 1 2))
+; FIRST
+
+(firstzero '(3 0 4))
+; SECOND
+
+(firstzero '(1 2 0))
+; THIRD
+
+(firstzero '(1 2 3))
+; NONE
+
+; (firstzero 5 4 0) ; invalid number of arguments: 3
+
+
+; 4.12 write a function CYCLE that cyclically counts from 1 to 99
+(defun cycle (n)
+  (cond 
+    ((>= n 99) 1)
+    ((> n 0) (+ n 1))))
+
+(cycle 1)
+; 2
+
+(cycle 2)
+; 3
+
+(cycle 98)
+; 99
+
+ (cycle 99)
+; 1
+
+ (cycle 105)
+; 1
+
+
+; 4.13 write a function HOWCOMPUTE that is the inverse of the defined COMPUTE function
+(defun compute (op x y)
+  (cond ((equal op 'sum-of) (+ x y))
+        ((equal op 'product-of) (* x y))
+        (t '(that does not compute))))
+
+(compute 'sum-of 3 7)
+; 10
+
+(compute 'product-of 2 4)
+; 8
+
+(compute 'zorch-of 3 1)
+; (THAT DOES NOT COMPUTE)
