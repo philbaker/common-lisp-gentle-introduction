bd9280e60a1d3a5b0827dc6636aa941c3407fea5
philbaker <phil@philbaker.me>
2023-09-02 13:49:07 +0100

    Continue with chapter 8 exercises

--- 08-recursion.lisp
+++ 08-recursion.lisp
@@ -262,3 +262,128 @@
 ; |   |   |   |   |   |   |   |   |   |     Arg-1 = -16855
 ; INFO: Control stack guard page unprotected
 ; Control stack guard page temporarily disabled: proceed with caution
+
+
+; 8.5. write an ADD-UP function by answering these questions
+; a.
+; - when do we stop? when the list is empty
+; - is there any list for which we immediately know hat the sum of all its
+; elements is? () or nil is equal to 0
+; - what is that list? ()
+; - what value should the function return if it gets that list as input: 0
+
+; b. do we know how to take a single step? base case: (null l) 0
+
+; c. how should ADD-UP call itself recursively to solve the rest of the
+; problem
+; (t (+ (first l) (testing-add (rest l))))
+; it will keep adding the first of the rest of the list to the total until
+; the list is empty
+
+(defun add-up (l)
+  (cond ((null l) 0)
+        (t (+ (first l) (add-up (rest l))))))
+
+; (dtrace add-up)
+
+(add-up '(2 3 7))
+; 12
+; ----Enter ADD-UP
+; |     Arg-1 = (2 3 7)
+; |   ----Enter ADD-UP
+; |   |     Arg-1 = (3 7)
+; |   |   ----Enter ADD-UP
+; |   |   |     Arg-1 = (7)
+; |   |   |   ----Enter ADD-UP
+; |   |   |   |     Arg-1 = NIL
+; |   |   |    \--ADD-UP returned 0
+; |   |    \--ADD-UP returned 7
+; |    \--ADD-UP returned 10
+;  \--ADD-UP returned 12
+
+
+; 8.6. write the function ALLODDP that returns T if all numbers in a list
+; are odd
+
+ (defun alloddp (l)
+   (cond ((null l) t)
+         ((evenp (first l)) nil)
+         (t (alloddp (rest l)))))
+
+(alloddp '(1 3 5 7))
+; T
+
+(alloddp '(1 3 4 7))
+; NIL
+
+; 8.7. write a recursive version of MEMBER
+
+(setf ducks '(huey dewey louie))
+; (HUEY DEWEY LOUIE)
+
+(member 'dewey ducks)
+; (DEWEY LOUIE)
+
+(defun rec-member (match l)
+  (cond ((null l) nil)
+        ((equal (first l) match) l)
+        (t (rec-member match (rest l)))))
+
+(rec-member 'huey ducks)
+; (HUEY DEWEY LOUIE)
+
+(rec-member 'dewey ducks)
+; (DEWEY LOUIE)
+
+(rec-member 'louie ducks)
+; (LOUIE)
+
+(rec-member 'kablouie ducks)
+; NIL
+
+
+; 8.8. write a recursive version of ASSOC
+(setf produce2
+      '((apple fruit) 
+        (celery veggie)
+        (banana fruit)
+        (lettuce veggie)))
+
+(assoc 'apple produce2)
+; (APPLE FRUIT)
+
+(defun assoc-rec (x l)
+   (cond ((null l) nil)
+         ((equal (first (first l)) x) (first l))
+         (t (assoc-rec x (rest l)))))
+
+(assoc-rec 'apple produce2)
+; (APPLE FRUIT)
+
+(assoc-rec 'celery produce2)
+; (CELERY VEGGIE)
+
+(assoc-rec 'banana produce2)
+; (BANANA FRUIT)
+
+(assoc-rec 'lettuce produce2)
+; (LETTUCE VEGGIE)
+
+(assoc-rec 'kiwi produce2)
+; NIL
+
+(assoc-rec 'fruit produce2)
+; NIL
+
+
+; 8.9. write a recursive version of NTH. Call it REC-NTH
+(nth 2 '(a b c))
+; C
+
+(defun rec-nth (i l)
+  (cond ((null l) nil)
+        ((equal i (first l)) (first l))
+        (t (rec-nth i (rest l)))))
+
+(rec-nth 2 '(a b c))
+
